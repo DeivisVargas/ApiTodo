31df92709af5607addf657b64b1b518c37739e94
Deivis_vargas <deivisvp@yahoo.com.br>
2023-11-27 16:36:55 -0300

    Adicionar arquivos de projeto.

---
+++ Todo.Domain/Entities/Entity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Todo.Domain.Entities
+{
+    //IEquatable<Entity> para poder comparar duas entidades 
+    public abstract class Entity : IEquatable<Entity>
+    {
+
+        public Entity()
+        {
+            //para gerar um novo id 
+            Id = Guid.NewGuid();
+        }
+
+
+        //para gerar id de auto complemento/chave unica 
+        //private ,ninguem de fora pode setar esse id ,somente a propria classe 
+        public Guid Id { get; private set; }
+
+
+
+        //overhide IEquatable
+        public bool Equals(Entity other)
+        {
+            return Id == other.Id;
+            //throw new NotImplementedException();
+        }
+    }
+}
